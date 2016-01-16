b3e7830cf501967e8051a24d5a304b48e5989c0d
Gautham Ramachandran <gautham2r@gmail.com>
2016-01-16 15:56:47 +0530

    Updates

--- test/object.js
+++ test/object.js
@@ -28,5 +28,43 @@ describe('test object cases', () => {
     expect(someObject.path('a.b')).to.equal(1);
     done();
   });
+  
+  it('should get all possible paths of an object', (done) => {
+    let someObject = {
+      a: 1,
+      b: { c: 1 },
+      d: { 
+        e: () => true
+      },
+      f: { 
+        g: {
+          h: 10,
+          z: 20
+        }
+      }
+    };
+    let result = someObject.paths();
+    expect(result).to.have.length(5);
+    expect(result[0].path).to.equal('a');
+    expect(result[result.length - 1].path).to.equal('f.g.z');
+    done();
+  });
+  
+  it('should get paths for nested object', (done) => {
+    let nestedObject = {};
+    for (var i = 10; i >= 0; i--) {
+      if (i === 10) {
+        nestedObject[`a${i}`] = i;
+      } else {
+        let obj = {};
+        obj[`a${i}`] = nestedObject;
+        nestedObject = obj;
+      }
+    }
+    let result = nestedObject.paths();
+    expect(result).to.have.length(1);
+    expect(result[0].value).to.equal(10);
+    done();
+  });
 });
 
